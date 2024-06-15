b4986b44fa98a3dd188a63e83a6111b859f8cae6
lexuantrung-Peter <le.xuan.trung@moneyforward.vn>
2024-06-15 17:13:11 +0700

    Allow downloading

--- src/components/qrCodes/generateWithLogo/index.tsx
+++ src/components/qrCodes/generateWithLogo/index.tsx
@@ -1,16 +1,40 @@
-import { type FC } from "react";
-import { QRCode } from 'react-qrcode-logo';
-
+import { type FC, useRef, useId } from "react";
+import * as htmlToImage from "html-to-image";
+import { QRCode } from "react-qrcode-logo";
 
 type GenerateQrCodeProps = {
   url: string;
   logoUrl?: string;
 };
 
-export const GenerateQrCodeWithLogo: FC<GenerateQrCodeProps> = ({ url, logoUrl }) => {
+export const GenerateQrCodeWithLogo: FC<GenerateQrCodeProps> = ({
+  url,
+  logoUrl,
+}) => {
+  const qrCodeRef = useRef(null);
+  const domainName = url.replace(
+    /(https?:\/\/)?(www\.)?([a-zA-Z0-9-]*).*$/,
+    "$3"
+  );
+  const id = useId();
+
+  const downloadQRCodeWithImage = () => {
+    const qrCodeCanvas = document.getElementById(id) as HTMLCanvasElement;
+    const link = document.createElement("a");
+    link.href = qrCodeCanvas.toDataURL();
+    link.download = `${domainName}.png`
+    link.click();
+  };
   return (
     <div className="form">
-      <QRCode value={url} size={300} logoImage={logoUrl}/>
+      <QRCode
+        value={url}
+        size={300}
+        logoImage={logoUrl}
+        ref={qrCodeRef}
+        id={id}
+      />
+      <button onClick={downloadQRCodeWithImage}>Tải QR Code</button>
     </div>
   );
 };
