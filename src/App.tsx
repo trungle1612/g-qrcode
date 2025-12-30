c6f253e83a673206d8db6d02dfe3eef047f0701a
lexuantrung-Peter <le.xuan.trung@moneyforward.vn>
2025-12-30 17:48:30 +0700

    Fix eslin

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
-import { useCallback, useState } from "react";
+import { useState } from "react";
 import "./App.css";
-import { GenerateQrCode } from "./components/qrCodes/generate";
 import { GenerateQrCodeWithLogo } from "./components/qrCodes/generateWithLogo";
 
 function App() {
-  type qrCodes = {
-    id: string;
-    url: string;
-  };
-
   const [url, setUrl] = useState("");
-  const [type, setType] = useState("directly");
-  const [isValid, setIsValid] = useState(true);
-  const [qrCodes, setQrCodes] = useState<qrCodes[]>([]);
   const [logoType, setLogoType] = useState("bao-tang");
   const [qrStyle, setQrStyle] = useState("squares");
   const [fgColor, setFgColor] = useState("01589d");
@@ -37,46 +28,6 @@ function App() {
     setFgColor(e.target.value);
   };
 
-  const handleOnClickFileType = () => {
-    // Read file from input
-    const input = document.querySelector(
-      "input[type=file]"
-    ) as HTMLInputElement;
-    const file = input?.files?.[0];
-    if (file) {
-      console.log(qrCodes);
-      setIsValid(true);
-    } else {
-      setIsValid(false);
-    }
-  };
-
-  const handleFileChange = useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {
-      const file = e.target.files?.[0];
-      setQrCodes([]);
-
-      if (file) {
-        const reader = new FileReader();
-        reader.onload = (e) => {
-          const data = e.target?.result;
-          const lines = data?.toString().split("\n");
-          // save lines.split(",") to qrCodes
-          lines?.forEach((line) => {
-            const [id, url] = line.split(",");
-            setQrCodes((prev) => [...prev, { id, url }]);
-          });
-        };
-        reader.readAsText(file);
-        setIsValid(true);
-      } else {
-        setIsValid(false);
-        setQrCodes([]);
-      }
-    },
-    []
-  );
-
   return (
     <div className="container">
       {url && (
@@ -93,13 +44,6 @@ function App() {
           qrStyle={qrStyle as "squares" | "dots" | "fluid"}
           fgColor={`#${fgColor}`}
         />
-      )}
-      {qrCodes.length > 0 && (
-        <div>
-          {qrCodes.map((qrCode) => (
-            <GenerateQrCode key={qrCode.id} url={qrCode.url} />
-          ))}
-        </div>
       )}
 
       <div className="form">
@@ -147,27 +91,10 @@ function App() {
             </select>
           </label>
         </div>
-        {type === "directly" && (
-          <>
-            <input placeholder="Enter URL" name="url" />
-            <button onClick={handleClick}>Tạo QrCode</button>
-          </>
-        )}
-        {type === "file" && (
-          <>
-            <span className="required-file" hidden={isValid}>
-              * Vui lòng chọn file
-            </span>
-            <input
-              type="file"
-              accept=".csv, .txt, .rtf"
-              onChange={handleFileChange}
-            />
-            <button onClick={handleOnClickFileType}>
-              Tạo danh sách QrCode
-            </button>
-          </>
-        )}
+        <>
+          <input placeholder="Enter URL" name="url" />
+          <button onClick={handleClick}>Tạo QrCode</button>
+        </>
       </div>
     </div>
   );
